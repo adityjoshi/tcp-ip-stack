f05cae520640119b876dba8867f5cffc859b345e
Aditya Joshi <aditya03joshi@gmail.com>
2024-12-19 22:45:13 +0530

    glueLL function

---
+++ gluethread/glthread.c
@@ -0,0 +1,11 @@
+#include "gltthread.h"
+#include "stdlib.h"
+
+void init_glthread(glthread_t *glthread) {
+ glthread -> left  = NULL;
+ glthread -> right = NULL;
+} 
+
+void glthread_add_next(glthread_t *base_glthread, glthread_t *new_glthread) {
+ if()
+}
