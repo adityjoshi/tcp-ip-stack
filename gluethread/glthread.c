c054a161e2a668e6e02114d3e59ee401e6097844
Aditya Joshi <aditya03joshi@gmail.com>
2024-12-19 23:00:17 +0530

    glueLL function

--- gluethread/glthread.c
+++ gluethread/glthread.c
@@ -6,6 +6,15 @@ void init_glthread(glthread_t *glthread) {
  glthread -> right = NULL;
 } 
 
-void glthread_add_next(glthread_t *base_glthread, glthread_t *new_glthread) {
- if()
+void glthread_add_next(glthread_t *curr_glthread, glthread_t *new_glthread) {
+ if(!curr_glthread->right) {
+    curr_glthread->right = new_glthread;
+    new_glthread->left = curr_glthread;
+return ;
+}
+glthread_t *temp = curr_glthread->right;
+curr_glthread->right = new_glthread; 
+new_glthread->left = curr_glthread; 
+new_glthread->right = temp; 
+temp->left=new_glthread; 
 }
