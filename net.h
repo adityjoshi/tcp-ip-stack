c4e0437fabcdad975015e65f66f1eaf84b79abac
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-01-16 00:20:56 +0530

    net.h

--- net.h
+++ net.h
@@ -3,6 +3,7 @@
 
 
 // add interface network properties and node network properties in graph.h 
+#include <_string.h>
 typedef struct ip_address_ {
   char ip_address[16];
 } ip_address_t ; 
@@ -15,9 +16,15 @@ typedef struct mac_address_ {
 typedef struct node_nw_properties_ {
   // layer 3 config 
   bool_t is_loopback_address_config ; 
-  ip_address_t lb_addr;
+  ip_address_t loopback_addr;
 } node_nw_properties_t;
 
+
+static inline void 
+init_node_nw_properties_(node_nw_properties_t *node_nw_prop) {
+  node_nw_prop->is_loopback_address_config = FALSE; 
+  memset(node_nw_prop->loopback_addr.ip_address,0,16);
+}
 
 
 
