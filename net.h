a54e8e3b2e0684f55246c015fa13797188727ed0
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-01-15 21:53:36 +0530

    net.h

--- net.h
+++ net.h
@@ -2,6 +2,18 @@
 #define __NET__
 
 
+// add interface network properties and node network properties in graph.h 
+typedef struct ip_address_ {
+  char ip_address[16];
+} ip_address_t ; 
+
+typedef struct mac_address_ {
+  char mac_address[8]; 
+} mac_address_t; 
+
+
+
+
 
 
 
