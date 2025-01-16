f05f5fce5bee4c3145d5276b97c0436b411c8926
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-01-16 13:32:38 +0530

    net.c

--- net.c
+++ net.c
@@ -3,6 +3,35 @@
 #include "graph.h"
 #include "net.h"
 
+
+static unsigned int
+hash_code(void *ptr, unsigned int size){
+    unsigned int value=0, i =0;
+    char *str = (char*)ptr;
+    while(i < size)
+    {
+        value += *str;
+        value*=97;
+        str++;
+        i++;
+    }
+    return value;
+}
+
+void interface_assign_mac_address(interface_t *interface) {
+node_t *node = interface->att_node;
+if(!node) {
+return ; 
+}
+unsigned int hashVal = 0 ; 
+    hashVal = hash_code(node->node_name, NODE_NAME_SIZE);
+    hashVal *= hash_code(interface->if_name, IF_NAME_SIZE);
+    memset(INTERFACE_MAC(interface), 0, sizeof(INTERFACE_MAC(interface)));
+    memcpy(INTERFACE_MAC(interface), (char *)&hashVal, sizeof(unsigned int));
+
+
+
+
 bool_t node_set_loopback_address(node_t *node, char*ip_addr ) {
 assert(ip_addr);
 node->node_network_prop->is_loopback_address_config = TRUE;
