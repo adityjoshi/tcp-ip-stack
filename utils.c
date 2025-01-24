bc5202fe06a2c8970e82d985fc9a3eed93067778
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-01-25 00:12:19 +0530

    mask creation utils.c

---
+++ utils.c
@@ -0,0 +1,20 @@
+#include "utils.h"
+#include <stdint.h>
+#include <string.h>
+
+
+void apply_mask(char *prefix, char mask, char *str_prefix) {
+
+uint32_t binary_prefix = 0 ;
+/*
+* 0x is for hexa decimal format and f is 1111 so basically its 255.255.255.255
+*/
+uint32_t subnetMask = 0xffffffff; 
+
+if (mask == 32) {
+strncpy(str_prefix,prefix,16);
+str_prefix[15] = '\0';
+return ; 
+}
+
+}
--- utils.h
+++ utils.h
@@ -9,6 +9,9 @@ TRUE
 }  bool_t ;
 
 
+void apply_mask(char *prefix, char mask, char *str_max) ;
+
+
 
 
 #endif /* __UTILS__ */
