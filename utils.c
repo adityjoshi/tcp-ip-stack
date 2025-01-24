1c2a682998e97e56236ba6c1112835c293a0edbc
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-01-25 00:38:19 +0530

    mask creation utils.c

--- utils.c
+++ utils.c
@@ -1,6 +1,8 @@
 #include "utils.h"
 #include <stdint.h>
 #include <string.h>
+#include <sys/_endian.h>
+#include <arpa/inet.h>
 
 
 void apply_mask(char *prefix, char mask, char *str_prefix) {
@@ -17,4 +19,6 @@ str_prefix[15] = '\0';
 return ; 
 }
 
+inet_pton(AF_INET, prefix, &binary_prefix);
+binary_prefix = htonl(binary_prefix);
 }
