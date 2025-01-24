ed680db1dc689c977108fb951ed100eb3a5b0f5e
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-01-25 00:50:11 +0530

    mask creation utils.c

--- utils.c
+++ utils.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sys/_endian.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 
 
 void apply_mask(char *prefix, char mask, char *str_prefix) {
@@ -19,9 +20,14 @@ str_prefix[15] = '\0';
 return ; 
 }
 
+// ipv4 ip converting 
 inet_pton(AF_INET, prefix, &binary_prefix);
 binary_prefix = htonl(binary_prefix);
 
 subnetMask = subnetMask << (32-mask);
 binary_prefix = binary_prefix & subnetMask ; 
+
+binary_prefix = htonl(binary_prefix);
+inet_ntop(AF_INET, &binary_prefix, str_prefix, 16);
+str_prefix[0] = '\0';
 }
