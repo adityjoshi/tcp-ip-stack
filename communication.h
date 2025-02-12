7452b0f70a828f9e74511b29eed2ba8961be5ddf
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-02-12 21:29:23 +0530

    communication.h

--- communication.h
+++ communication.h
@@ -4,13 +4,14 @@
 
 /*
 *
-*
-*
+* the api is used to send the packet to the neighbour node
+* neighbour node which is connected through the link to the original node should receive the packet 
 *
 *
 */
 
 int send_packet_out(char *pkt, unsigned int pkt_size, interface_t *original_intf);
+
 
 
 #endif  /* __COMMUNICATION__ */
