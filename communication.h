c628f755f48fc3bfffc93c0e2b04a64289c04910
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-02-12 21:28:01 +0530

    communication.h

---
+++ communication.h
@@ -0,0 +1,16 @@
+#ifndef __COMMUNICATION__
+#define __COMMUNICATION__
+#include "graph.h"
+
+/*
+*
+*
+*
+*
+*
+*/
+
+int send_packet_out(char *pkt, unsigned int pkt_size, interface_t *original_intf);
+
+
+#endif  /* __COMMUNICATION__ */
