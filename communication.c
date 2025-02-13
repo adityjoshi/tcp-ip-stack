6160fc1c596bf63d2e23bd93f9654eb48439bfac
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-02-13 20:44:25 +0530

    udp socket

--- communication.c
+++ communication.c
@@ -1,4 +1,7 @@
 #include "communication.h"
+#include "graph.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 static unsigned int udp_num = 40000;
 
@@ -13,7 +16,23 @@ void init_udp_socket(node_t *node) {
         return ; 
     }
     node->udp_port_number = get_udp_port();
-    
+    int udp_socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_socket_fd == -1) {
+        printf("Socket Creation Failed for node %s\n", node->node_name);
+        return;   
+    }
+
+    struct sockaddr_in node_addr;
+    node_addr.sin_family      = AF_INET;
+    node_addr.sin_port        = node->udp_port_number;
+    node_addr.sin_addr.s_addr = INADDR_ANY;
+    if (bind(udp_socket_fd, (struct sockaddr *)&node_addr, sizeof(struct sockaddr)) == -1) {
+        printf("Error : socket bind failed for Node %s\n", node->node_name);
+        return;
+    }
+
+    node->udp_socket_fd = udp_socket_fd;
+    return ;
 }
 
 int send_packet_out(char *pkt, unsigned int pkt_size, interface_t *original_intf) {
