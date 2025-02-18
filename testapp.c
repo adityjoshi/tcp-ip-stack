b6aa044cb78724d62d8041dbfc10c80eac0ed8b9
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-02-18 09:08:20 +0530

    comm test

--- testapp.c
+++ testapp.c
@@ -10,11 +10,7 @@ int main(int argc, char **argv) {
 
 nw_init_cli();
 topo = build_first_topo();
-// node_t *R0_re = create_new_node(topo, "R0");
-// node_t *R1_re = create_new_node(topo, "R1");
-// node_t *R2_re = create_new_node(topo, "R2");
 
-//dump_graph(topo);
 sleep(2);
 node_t *snode = get_node_by_node_name(topo, "R0_re");
 interface_t *oif = get_node_if_by_name(snode, "eth0/0");    
