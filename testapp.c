5d4a5b528257e0d51f96b9049f95a77cdd925d3c
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-02-19 19:30:53 +0530

    3-node-linear-topo

--- testapp.c
+++ testapp.c
@@ -3,23 +3,14 @@
 #include "communication.h"
 #include "unistd.h"
 extern graph_t *build_first_topo();
+extern graph_t *build_linear_topo();
 extern void nw_init_cli();
 graph_t *topo = NULL ;
 
 int main(int argc, char **argv) {
   
 nw_init_cli();
-topo = build_first_topo();
-
-sleep(2);
-node_t *snode = get_node_by_node_name(topo, "R0_re");
-interface_t *oif = get_node_if_by_name(snode, "eth0/0");    
-char msg [] = "Hell";
-
-send_packet_out(msg, strlen(msg), oif);
-
-
-
+topo = build_linear_topo();
 
 
 start_shell();
--- topologies.c
+++ topologies.c
@@ -24,19 +24,7 @@ graph_t * build_first_topo() {
      +-------+                                              +----------+
 #endif /* if 0 */
 
-// graph_t *topo = create_new_graph("Generic Graph");
-// node_t *R0_re = create_new_node(topo,"R0_re");
-// node_t *R1_re = create_new_node(topo,"R1_re");
-// node_t *R2_re = create_new_node(topo,"R2_re");
 
-// insert_link_between_node(R0_re,R1_re,"eth0/0","eth0/1",1);
-// insert_link_between_node(R1_re, R2_re, "eth0/2", "eth0/3", 1);
-// insert_link_between_node(R0_re, R2_re, "eth0/4", "eth0/5", 1);
-
-// node_set_loopback_address(R0_re, "127.1.1.0");
-// node_set_interface_ip_address(R0_re, "eth0/0", "40.1.1.1",24);
-
-// return topo;
 
     graph_t *topo = create_new_graph("Hello World Generic Graph");
     node_t *R0_re = create_new_node(topo, "R0_re");
@@ -63,4 +51,43 @@ graph_t * build_first_topo() {
     // network_start_packet_receiver_thread(topo);
     return topo;
 // new topo
+}
+
+
+
+
+graph_t * build_linear_topo() {
+
+
+#if 0
+
+       +----------+                      +----------+                      +----------+
+       |          |0/1                0/2|          |0/3        20.1.1.2/24 |           |
+ ------+   R0_re  +----------------------+   R1_re  +----------------------+  R2_re    +
+       | 122.1.1.1|10.1.1.1/24           |122.1.1.2 |20.1.1.2/24        0/4|122.1.1.3  |
+       +----------+           10.1.1.2/24+----------+                      +-----------+
+
+
+
+
+#endif /* if 0 */
+graph_t *topo = create_new_graph("3 Node linear topo");
+node_t *R0_re = create_new_node(topo, "R0_re");
+node_t *R1_re = create_new_node(topo, "R1_re");
+node_t *R2_re = create_new_node(topo, "R2_re");
+
+node_set_loopback_address(R0_re, "122.1.1.1");
+node_set_loopback_address(R1_re, "122.1.1.2");
+node_set_loopback_address(R2_re, "122.1.1.3");
+
+insert_link_between_node(R0_re, R1_re, "eth0/1", "eth0/2", 1);
+insert_link_between_node(R1_re, R2_re, "eth0/3", "eth0/4", 1);
+
+node_set_interface_ip_address(R0_re, "eth0/1", "10.1.1.1",24);
+node_set_interface_ip_address(R1_re, "eth0/2", "10.1.1.2",24);
+node_set_interface_ip_address(R1_re, "eth0/3", "20.1.1.1",24);
+node_set_interface_ip_address(R2_re, "eth0/4", "20.1.1.2",24);
+
+return topo;
+
 }
