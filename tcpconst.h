a7b53617bd7dd3afe0db33e674084188197d5e93
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-02-26 12:18:05 +0530

    arpHeader

---
+++ tcpconst.h
@@ -0,0 +1,13 @@
+#ifndef __TCPCONST__
+#define __TCPCONST__
+
+#define ARP_BROAD_REQ 1
+#define ARP_REPLY 2
+#define ARP_MESSAGE 806
+#define ARP_BROADCAST_MAC 0xFFFFFFFFFFFF
+
+
+
+
+
+#endif /* __TCPCONST__ */
