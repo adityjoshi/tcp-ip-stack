9f4419d44db40c9d8da17b31c291d82a63e56555
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-02-26 22:43:19 +0530

    arp table entry addition

--- layer2/layer2.c
+++ layer2/layer2.c
@@ -4,6 +4,12 @@
 
 
 
+void
+init_arp_table(arp_table_t **arp_table){
+
+    *arp_table = calloc(1, sizeof(arp_table_t));
+    init_glthread(&((*arp_table)->arp_entries));
+}
 
 
 
@@ -16,5 +22,14 @@ init_arp_table(arp_table_t **arp_table){
 
 
 void arp_table_entry_lookup(arp_table_t *arp_table, char *ip_addr) {
-    
+    glthread_t *curr;
+    arp_entries_t *arp_entry;
+
+    ITERATE_GLTHREAD_BEGIN(&arp_table->arp_entries,curr) {
+        arp_entry = arp_glue_to_arp_entry(curr);
+        if (strcmp(arp_entry->ip_address.ip_address, ip_addr) == 0) {
+            return arp_entry;
+        }
+    } ITERATE_GLTHREAD_END(&arp_table->arp_entries,curr);
+    return NULL ; 
 }
