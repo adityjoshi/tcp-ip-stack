74f2fa7096cba231fbc58d7b76ba4c4946149a99
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-03-06 10:07:36 +0530

    arp table entry addition

--- layer2/layer2.c
+++ layer2/layer2.c
@@ -41,3 +41,16 @@ void delete_arp_entry(arp_table_t *arp_table, char *ip_addr) {
     }
     // add delete arp_entry(arp_entry);
 }
+
+
+bool_t arp_table_entry_addition(arp_table_t *arp_table, arp_entries_t *arp_entry) {
+    arp_entries_t *arp_entry_old = arp_table_entry_lookup(arp_table,arp_entry->ip_address.ip_address);
+
+    if (arp_entry_old && memcpy(arp_entry_old, arp_entry, sizeof(arp_entries_t)) == 0) {
+        return FALSE;
+    }
+
+    if (arp_entry_old) {
+        delete_arp_entry(arp_table, arp_entry->ip_address.ip_address);
+    }
+}
