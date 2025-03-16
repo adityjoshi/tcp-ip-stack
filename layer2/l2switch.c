b8a08220d4865c7b200b3302f1634dedbc875545
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-03-16 10:20:50 +0530

    mac table struct

--- layer2/l2switch.c
+++ layer2/l2switch.c
@@ -1,8 +1,14 @@
 #include "glthread.h"
+#include "net.h"
+#include "graph.h"
 
 
 
-
+typedef struct mac_table_entries {
+    mac_address_t mac_address;
+    char oif_name[IF_NAME_SIZE];
+    glthread_t mac_glue;
+} mac_table_entries_t;
 
 
 
