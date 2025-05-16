dab293472c480c17739602e860fb6048c9b79a66
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-05-16 16:14:29 +0530

    vlan-tag

--- layer2/layer2.c
+++ layer2/layer2.c
@@ -370,6 +370,8 @@ ethernetHeader_t *tag_pkt_with_vlan_id(ethernetHeader_t *ethernet_hdr, unsigned 
 
 ethernetHeader_t *untag_pkt_with_vlan_id(ethernetHeader_t *ethernet_hdr, unsigned int total_pkt_size,  unsigned int *new_pkt_size) {
     *new_pkt_size = 0;
+
+    vlan_8021q_hdr_t *vlan_8021q_hdr = is_pkt_vlan_tagged(ethernet_hdr);
 }
                      
                    
