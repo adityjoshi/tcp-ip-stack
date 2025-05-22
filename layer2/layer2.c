3c58023c152c71ca2c3571f2dc0a6d729eee33ed
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-05-22 12:22:56 +0530

    vlan

--- layer2/layer2.c
+++ layer2/layer2.c
@@ -372,6 +372,8 @@ void node_set_intf_l2_mode(node_t *node, char *intf_name, intf_l2_mode_t intf_l2
 
 void node_set_intf_vlan_membership(node_t *node, char *intf_name, unsigned int vlan_id) {
     interface_t *interface = get_node_if_by_name(node, intf_name);
+    assert(interface);
+  interface_set_vlan(node, interface, vlan_id);
 
 }
 
