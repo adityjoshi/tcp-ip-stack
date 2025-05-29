ea18e0b796aea3406091d272316b59bb6ebf2cea
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-05-29 12:05:27 +0530

    recv_qualify_on_interface

--- layer2/layer2.h
+++ layer2/layer2.h
@@ -162,18 +162,7 @@ static inline bool_t l2_frame_recv_qualify_on_interface(interface_t *interface, 
         return FALSE;
      }
      
-    
-    // if(!IS_INTF_L3_MODE(interface)) {
-    //     return FALSE;
-    // }
-    // if (memcpy(INTERFACE_MAC(interface),ethernetHeader->dest.mac_address,sizeof(mac_address_t)) == 0) {
-    //     return TRUE;
-    // }
-    // if (IS_MAC_BROADCAST_ADDR(ethernetHeader->dest.mac_address)) {
-    //     return TRUE ;
-    // }
-
-    // return FALSE ;
+ 
 
 }
 
