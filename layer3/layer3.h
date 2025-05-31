47eb199e7b92e3c724b03a61fe4494cc17b9f23a
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-05-31 13:54:36 +0530

    layer 3

--- layer3/layer3.h
+++ layer3/layer3.h
@@ -2,10 +2,23 @@
 #define __LAYER3__
 
 
+#include <glthread.h>
+#include "utils.h"
+#include "graph.h"
+
+typedef struct rt_table {
+    glthread_t route_list; 
+} rt_table_t ; 
 
 
-
-
+typedef struct L3_route {
+    char dest[16] ; 
+    char mask ;
+    bool_t is_direct ; /* for local route and direct route*/
+    char gw_ip[16]; /* next gateway ip */
+    char oif_name[IF_NAME_SIZE]; /* outgoing interface name */
+    glthread_t route_glue;
+} L3_route_t;
 
 
 
