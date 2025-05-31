0fe483362583a2b741c49cfc292195d1d1878190
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-05-31 06:48:17 +0530

    vlan fixed

--- testapp.c
+++ testapp.c
@@ -6,13 +6,17 @@ extern graph_t *build_first_topo();
 extern graph_t *build_linear_topo();
 extern graph_t *build_simple_l2_switch_topo();
 extern graph_t *build_dualswitch_topo();
+extern graph_t *L2_loop_topo();
 extern void nw_init_cli();
+
+
+
 graph_t *topo = NULL ;
 
 int main(int argc, char **argv) {
   
 nw_init_cli();
-topo = build_dualswitch_topo();
+topo = L2_loop_topo();
 
 
 start_shell();
