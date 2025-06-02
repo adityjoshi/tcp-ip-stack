cd1a19f88d8b17f924761c8c8bb7dd2d57f41054
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-06-02 21:50:41 +0530

    l5

--- layer5/layer5.c
+++ layer5/layer5.c
@@ -11,5 +11,8 @@
 void layer5_ping(node_t *node,char *dst_ip_addr) {
     unsigned int addr_int ; 
     printf("Src node : %s, sending ping to %s\n", node->node_name, dst_ip_addr);
+
+    inet_pton(AF_INET, dst_ip_addr, &addr_int);
+    addr_int = htonl(addr_int);
     
 }
